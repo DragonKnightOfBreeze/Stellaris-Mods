7f3816e6a306f3c7013f4819e64dca781626002e
DragonKnightOfBreeze <2944262241@qq.com>
2020-06-06 19:19:07 +0800

    添加新的种族肖像和载入图片。 添加新起源：共同进化。

--- kitsune/src/main/kotlin/ScriptGenerator.kt
+++ kitsune/src/main/kotlin/ScriptGenerator.kt
@@ -7,72 +7,62 @@ fun main() {
 }
 
 private fun generatePortraitsScript() {
-	val kitsuneIdeaCount = 32
-	val kitsuneCount = 52
+	val filePath = "kitsune/package/gfx/portraits/portraits/kitsune_portraits.txt"
+	val fileNames:Array<String> = File("kitsune/package/gfx/models/portraits/kitsune").list() ?: arrayOf()
+	val defaultRefText = "idea_1"
+	val refText = fileNames.joinToString("\n") {
+		it.substringBefore(".")
+	}
+	val defText = fileNames.joinToString("\n"){
+		"${it.substringBefore(".")} = {texturefile = \"gfx/models/portraits/kitsune/$it\"}"
+	}
 
-	val kitsuneDef = buildString {
-		appendln("kitsune_idea = {texturefile = \"gfx/models/portraits/kitsune/kitsune_idea.dds\"}")
-		repeat(kitsuneIdeaCount) {
-			appendln("kitsune_idea_$it = {texturefile = \"gfx/models/portraits/kitsune/kitsune_idea_$it.dds\"}")
-		}
-		repeat(kitsuneCount) {
-			appendln("kitsune_$it = {texturefile = \"gfx/models/portraits/kitsune/kitsune_$it.dds\"}")
-		}
-	}.trimEnd()
-	val kitsuneRef = buildString {
-		appendln("kitsune_idea")
-		repeat(kitsuneIdeaCount) {
-			appendln("kitsune_idea_$it")
-		}
-		repeat(kitsuneCount) {
-			appendln("kitsune_$it")
-		}
-	}.trimEnd()
 	val text = """
 portraits = {
-${kitsuneDef.prependIndent("\t")}
+${defText.prependIndent("\t")}
 }
 
 portrait_groups = {
 	kitsune = {
-		default = kitsune_idea
+		default = $defaultRefText
 		game_setup = {
 			add = {
 				portraits = {
-${kitsuneRef.prependIndent("\t".repeat(5))}
+${refText.prependIndent("\t".repeat(5))}
 				}
 			}
 		}
 		species = {
 			add = {
 				portraits = {
-${kitsuneRef.prependIndent("\t".repeat(5))}
+${refText.prependIndent("\t".repeat(5))}
 				}
 			}
 		}
 		pop = {
 			add = {
 				portraits = {
-${kitsuneRef.prependIndent("\t".repeat(5))}
+${refText.prependIndent("\t".repeat(5))}
 				}
 			}
 		}
 		leader = {
 			add = {
 				portraits = {
-${kitsuneRef.prependIndent("\t".repeat(5))}
+${refText.prependIndent("\t".repeat(5))}
 				}
 			}
 		}
 		ruler = {
 			add = {
 				portraits = {
-${kitsuneRef.prependIndent("\t".repeat(5))}
+${refText.prependIndent("\t".repeat(5))}
 				}
 			}
 		}
 	}
 }
 	""".trimIndent()
-	File("kitsune/package/gfx/portraits/portraits/kitsune_portraits.txt").writeText(text)
+
+	File(filePath).writeText(text)
 }
